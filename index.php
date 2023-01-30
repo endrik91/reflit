f7484d5abf0dbaa61bb434c42a5a0f4a322853a3
endrik91 <123976611+endrik91@users.noreply.github.com>
2023-01-31 00:25:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Puskesmas Kedaung Barat</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs
+	/font-awesome/6.2.1/css/all.min.css">
+
+ 
+
+    		<div class="medsos">
+			<div class="container">
+	<ul>
+		<li><a href="#"><i class="fa-brands fa-facebook"></i></li>
+		<li><a href="#"><i class="fa-brands fa-instagram"></i></li>
+		<li><a href="#"><i class="fa-brands fa-youtube"></i></li>
+	</ul>
+			</div>
+			</div>
+   
+</head>
+
+
+<body>
+        
+
+		<header>
+		<div class="container">	
+			<h2> <a href="#">PUSKESMAS KEDAUNG BARAT</a></h2>
+		   
+				<ul>
+					<li class="active"><a href="#">HOME</a></li>
+					<li><a href="#">ABOUT</a></li>
+					<li><a href="#">CONTACK ME</a></li>
+					<li><a href="#">SERVICES</a></li>
+					
+				</ul>
+				</div>
+
+
+		</header>
+				<section class="banner">
+				 <img src="img/kabupaten.png" style="width:200px;  top:0px " align="right"/>
+				           <h2>WELCOME PUSKESMAS KEDAUNG BARAT KEC. SEPATAN TIMUR</h2>
+				</section>		
+<section class="about">
+	<div class="container">
+		<h3>ABOUT</h3>
+	</div>
+</section>
+
+<section class="contack me">
+	<div class="container">
+		<h3>CONTACK ME</h3>
+	</div>
+</section>
+
+<section class="services">
+	<div class="container">
+		<h3>SERVICES</h3>
+	</div>
+</section
+
+		
+
+</body>
+	</div>
+		</div>				
+			</html>
